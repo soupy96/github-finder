55bcefcaf4509f53ed2d28fadb805cf134ca05f7
Michael Campbell <michael.campbell14@gmail.com>
2020-08-24 14:11:41 -0400

    lesson 9, conditionals

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
   render () {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+
     return (
       <div className='App'>
-        <h1>Hello from React</h1>
-        <h2>Goodbye</h2>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </div>
     );
   }
