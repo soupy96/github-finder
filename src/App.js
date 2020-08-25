1ea9ca4fafab8f9d32357269d787d81d8b906676
Michael Campbell <michael.campbell14@gmail.com>
2020-08-25 17:13:57 -0400

    lesson 18, passing props up & search, it isnt bringing back results so im going to look into it

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
+  // search github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({ users: res.data, loading: false });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    console.log('object')
+    this.setState({ users: res.data.items, loading: false });
+    console.log('object2')
   }
 
   render () {
@@ -24,7 +26,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
