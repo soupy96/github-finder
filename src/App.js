47ad8c3ef5f915a052fbc63564868e06845bb042
Michael Campbell <michael.campbell14@gmail.com>
2020-08-25 17:26:35 -0400

    fixed the issue i was having in lesson 18 where the users werent appearing when searched. i missed a '&' after the text input

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ class App extends Component {
   }
 
   // search github users
-  searchUsers = async (text) => {
+  searchUsers = async text => {
     this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    console.log('object')
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    console.log(res)
+
     this.setState({ users: res.data.items, loading: false });
-    console.log('object2')
   }
 
   render () {
