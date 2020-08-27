2246a15c6cda6545fc8331c732c7b43defd95347
Michael Campbell <michael.campbell14@gmail.com>
2020-08-27 14:03:48 -0400

    lesson 30, create reducer & actions

--- src/App.js
+++ src/App.js
@@ -17,16 +17,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // search github users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  }
-
   // get a single github user
   const getUser = async (username) => {
     setLoading(true);
@@ -69,7 +59,7 @@ const App = () => {
             <Switch>
               <Route exact path='/' render={props => (
                 <Fragment>
-                  <Search setAlert={showAlert} clearUsers={clearUsers} searchUsers={searchUsers} showClear={users.length > 0 ? true : false} />
+                  <Search setAlert={showAlert} clearUsers={clearUsers} showClear={users.length > 0 ? true : false} />
                   <Users loading={loading} users={users} />
                 </Fragment>
               )} />
