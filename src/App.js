fbc33080d7c7d3d270e82b55348c53d0c1eb8e3a
Michael Campbell <michael.campbell14@gmail.com>
2020-08-27 14:51:17 -0400

    cleaning up some code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+
 import './App.css';
 
 const App = () => {
